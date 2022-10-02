563336abe2ba18558bd95f20c7b6a59cfc2e6d79
AldanaMicaelaFiliberto <aldana.filiberto17@gmail.com>
2022-10-02 16:36:18 -0300

    rellene clase arista

--- cluster-AGM/src/grafos/Arista.java
+++ cluster-AGM/src/grafos/Arista.java
@@ -1,5 +1,16 @@
 package grafos;
 
 public class Arista {
+	int verticeInicio;
+	int verticeDestino;
+	int peso;
 
+
+	public Arista(int vertice1, int vertice2, int peso) {
+		
+		this.verticeInicio=vertice1;
+		this.verticeDestino=vertice2;
+		this.peso=peso;
+		
+	}
 }
