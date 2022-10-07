7fa4bc4f9a08896c9e1d703e75a38282bc22f44c
AldanaMicaelaFiliberto <aldana.filiberto17@gmail.com>
2022-10-07 17:21:13 -0300

    nada

--- cluster-AGM/src/grafos/ClusterAGM.java
+++ cluster-AGM/src/grafos/ClusterAGM.java
@@ -5,7 +5,6 @@ public class ClusterAGM {
 	
 	public static Grafo cluster(Grafo g, int cantClusters) {
 		int i= 1;
-		
 		while(i<cantClusters) {
 			g.eliminarArista(g.getAristaMasPesada()[0], g.getAristaMasPesada()[1]);
 			i++;
